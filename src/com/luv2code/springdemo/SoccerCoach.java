4992177f2cf9bf9701083112aa8912652495c7ca
Marouane Ahal <d144013152@gmail.com>
2022-06-25 12:23:20 +0100

    dependency injection

--- src/com/luv2code/springdemo/SoccerCoach.java
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -2,7 +2,13 @@ package com.luv2code.springdemo;
 
 public class SoccerCoach implements Coach {
 
+    @Override
     public String getDailyWorkout() {
         return "Score 3 goals";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return null;
     }
 }
